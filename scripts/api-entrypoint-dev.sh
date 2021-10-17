48147cd5204cbdb5aa9b19f8365ac275e1056c8a
David <dusek.d@seznam.cz>
2021-10-17 13:35:51 +0200

    init db entrypoint

--- scripts/api-entrypoint-dev.sh
+++ scripts/api-entrypoint-dev.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+echo -e "Init database"
+python /finances_summary/_sync/init_db.py
+
 echo -e "Running webserver"
 python /finances_summary/app.py
--- src/finances_summary/_sync/init_db.py
+++ src/finances_summary/_sync/init_db.py
@@ -8,11 +8,13 @@ from finances_summary.settings import MONGO_CONN_URI
 connect(host=MONGO_CONN_URI)
 
 
-def _init_stocks(csv_file: str) -> tuple[int, int, int]:
+def _init_stocks(csv_file: str, skip_header=False) -> tuple[int, int, int]:
     skipped, added, failed = 0, 0, 0
     with open(csv_file, 'r', encoding='utf-8') as file:
         reader = csv.reader(file, delimiter=',')
-        next(reader)  # Skip header
+        if skip_header:
+            next(reader)  # Skip header
+        print(f'adding stocks...')
         for row in reader:
             commodity = Commodities(symbol=row[0],
                                     name=row[1],
@@ -20,13 +22,13 @@ def _init_stocks(csv_file: str) -> tuple[int, int, int]:
             try:
                 commodity.save()
                 added += 1
-                print(f'added stock {row[0]} - {row[1]}')
-            except NotUniqueError as e:
+                # print(f'added stock {row[0]} - {row[1]}')
+            except NotUniqueError:
                 skipped += 1
-                print(f'skipped stock {row[0]} - {row[1]}')
-            except (OperationError, ValidationError) as e:
+                # print(f'skipped stock {row[0]} - {row[1]}')
+            except (OperationError, ValidationError):
                 failed += 1
-                print(f'failed stock {row[0]} - {row[1]}\nerr: {e}')
+                # print(f'failed stock {row[0]} - {row[1]}\nerr: {e}')
         return (skipped, added, failed)
 
 
@@ -34,27 +36,29 @@ def _init_crypto(json_file: str) -> tuple[int, int, int]:
     skipped, added, failed = 0, 0, 0
     with open(json_file, 'r', encoding='utf-8') as file:
         data = json.load(file)
-        for symbol, name in data.items():
+        items = data.items()
+        print(f'adding {len(items)} cryptocurrencies...')
+        for symbol, name in items:
             commodity = Commodities(symbol=symbol,
                                     name=name,
                                     commodity_type=CommodityType.CRYPTO.name)
             try:
                 commodity.save()
                 added += 1
-                print(f'added crypto {symbol} - {name}')
+                # print(f'added crypto {symbol} - {name}')
             except NotUniqueError:
                 skipped += 1
-                print(f'skipped crypto {symbol} - {name}')
+                # print(f'skipped crypto {symbol} - {name}')
             except (OperationError, ValidationError) as e:
                 failed += 1
-                print(f'failed crypto {symbol} - {name}\nerr: {e}')
+                # print(f'failed crypto {symbol} - {name}\nerr: {e}')
         return (skipped, added, failed)
 
 
 def init_db() -> None:
     """Init database data.
     """
-    stocks_result = _init_stocks('finances_summary/_sync/data/symbols-stocks.csv')
+    stocks_result = _init_stocks('finances_summary/_sync/data/symbols-stocks.csv', True)
     crypto_result = _init_crypto('finances_summary/_sync/data/symbols-crypto.json')
     print(f'''\nstocks:
     skipped {stocks_result[0]}

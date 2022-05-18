563da2394b66dcc1f1d5d03b27f7766fdd6f8ccd
David <dusek.d@seznam.cz>
2022-05-19 01:13:42 +0200

    homepage add widgets

--- src/frontend/src/pages/Home.tsx
+++ src/frontend/src/pages/Home.tsx
@@ -1,20 +1,55 @@
 import styled from 'styled-components';
-import { COLOR_DARK_GREY } from '../utils/cssConstants';
 import AddTransactionForm from '../components/forms/AddTransaction';
+import TransactionsHistory from '../components/TransactionsHistory';
 
-const Container = styled.div`
-  background-color: ${COLOR_DARK_GREY};
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 50%);
+  /* grid-template-rows: repeat(2, 600px); */
+  grid-gap: 50px 0;
+  margin: 20px;
+`;
+
+const GridItemOne = styled.div`
+  grid-column: 1;
+  grid-row: 1;
+`;
+
+const GridItemTwo = styled.div`
+  grid-column: 2;
+  grid-row: 1;
+`;
+
+const GridItemThree = styled.div`
+  grid-column-start: 1;
+  grid-column-end: 3;
+`;
+
+const GridItemFour = styled.div`
+  grid-column: 1;
+  grid-row: 3;
 `;
 
 const Home = () => {
   return (
-    <Container>
-      <div>
+    <Grid>
+      <GridItemOne>
+        <h2>Add new transaction</h2>
         <AddTransactionForm />
+      </GridItemOne>
+      <GridItemTwo>
+        <h2>someying</h2>
         <AddTransactionForm />
-      </div>
-      <span>home</span>
-    </Container>
+      </GridItemTwo>
+      <GridItemThree>
+        <h2>Last transactions</h2>
+      <TransactionsHistory />
+      </GridItemThree>
+      <GridItemFour>
+        <h2>Something something</h2>
+        <span>asdf</span>
+      </GridItemFour>
+    </Grid>
   );
 };
 
